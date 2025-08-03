76dd39381700e7f1a3ae2cafb78ced2b327d5e3e
Franklin <franklin.oliveira096@gmail.com>
2025-08-03 16:51:05 -0300

    adicionando imagem contato

--- contact/forms.py
+++ contact/forms.py
@@ -4,32 +4,17 @@ from contact.models import Contact
 
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'placeholder': 'Seu nome',
-            }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda'
-    ) 
-
-    def __init__(self, *args, **kwargs ):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #     'placeholder':'Olha aqui',
-        # })
+    picture = forms.ImageField(
+         widget=forms.FileInput(
+              attrs={
+                   'accept': 'image/*',
+              }
+         )
+    )
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name', 'phone','email','description','category')
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'placeholder': 'Escreva aqui',
-        #         }
-        #     )
-        # }
+        fields = ('first_name', 'last_name', 'phone','email','description','category', 'picture',)
+
     def clean(self):
         first_name = self.cleaned_data.get('first_name')
         last_name = self.cleaned_data.get('last_name')
@@ -41,22 +26,7 @@ class ContactForm(forms.ModelForm):
                 )
                 self.add_error('last_name', msg)
                 self.add_error('first_name', msg)
-
-        # self.add_error(
-        #     'first_name',
-        #     ValidationError(
-        #         'Mensagem de erro',
-        #         code='invalid'
-        #     )
-        # )
-        # self.add_error(
-        #     'first_name',
-        #     ValidationError(
-        #         'Mensagem de erro2',
-        #         code='invalid'
-        #     )
-        # )
-    
+        return super().clean()
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
         if first_name == 'ABC':
